fe48ac62a2b6a2b9840fa00ed73d1252a4b28ef6
Kathlyn Cabrera <kicabrer@uci.edu>
2019-05-19 20:06:30 -0700

    Edited server.js main.js and lobby.html to enable chatting

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 /* Functions for general use */
 
 /* This function returns the value associated with whichParam on the URL */
+
 function getURLParameters(whichParam)
 {
 	var pageURL = window.location.search.substring(1);
@@ -21,4 +22,48 @@ if('undefined' == typeof username || !username)
 	username = 'Anon_'+Math.random();
 }
 
-$('#messages').append('<h3>Welcome '+username+'!</h3>');
+var chat_room = 'The_Chat_Room'
+
+
+/* Connect to the socket server */
+
+var socket = io.connect();
+
+socket.on('log',function(array){
+	console.log.apply(console,array);
+});
+
+socket.on('join_room_response', function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	$('#messages').append('<p>New user: '+payload.username+' joined the room.</p>');
+});
+
+socket.on('send_message_response', function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	$('#messages').append('<p><b>'+ payload.username +' says:</b> '+ payload.message +'</p>');
+});
+
+function send_message(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+	payload.message = $('#send_message_holder').val();
+	console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
+	socket.emit('send_message',payload);
+};
+
+
+$(function(){
+	var payload = {};
+	payload.room = chat_room;
+	payload.username = username;
+
+	console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
+	socket.emit('join_room',payload);
+});
